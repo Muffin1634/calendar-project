899a87b31f00a9f632a008c33876d810260fa2db
zandyisrad <34150410+zandyisrad@users.noreply.github.com>
2018-06-12 16:50:17 -0700

    Update client.py

--- client/client.py
+++ client/client.py
@@ -2,13 +2,9 @@ import connection
 
 commands = ["upload","download","calendar","save","move","rename"]
 
-def returnError():
-    pass
-
-while True:
-    data = input(">> ")
+def sendwithresponse(data):
     testdata = data.split()
     if testdata[0] in commands:
-        connection.send(data)
+        return connection.send(data)
     else:
-        returnError()
+        return("error")
