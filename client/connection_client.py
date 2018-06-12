4b85f087f4a39043e117d9fb6650b58c27239b11
zandyisrad <34150410+zandyisrad@users.noreply.github.com>
2018-06-12 16:54:48 -0700

    Update and rename connection-client.py to connection_client.py

--- client/connection_client.py
+++ client/connection_client.py
@@ -2,7 +2,7 @@ import socket, pickle
 
 def send(data, ip):
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.connect((input("IP Address of server: "), 4000))
+	server.connect(ip, 4000))
 	data = pickle.dumps(data)
 	server.send(data)
 	data = server.recv(1024)
