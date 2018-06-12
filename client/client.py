9c643341d9d38f2f007bef1995f94c9da4513278
Muffin <muffin.1634@gmail.com>
2018-06-11 20:10:10 -0700

    Update client.py
    keep tabs consistent

--- client/client.py
+++ client/client.py
@@ -3,12 +3,12 @@ import connection
 commands = ["upload","download","calendar","save","move","rename"]
 
 def returnError():
-  pass
+    pass
 
 while True:
-  data = input(">> ")
-  testdata = data.split()
-  if testdata[0] in commands:
-    connection.send(data)
-  else:
-    returnError()
+    data = input(">> ")
+    testdata = data.split()
+    if testdata[0] in commands:
+        connection.send(data)
+    else:
+        returnError()
