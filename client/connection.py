48bdc107cac1cfcaf2e9209e9d351756f26e9dd0
zandyisrad <34150410+zandyisrad@users.noreply.github.com>
2018-06-11 18:59:08 -0700

    Create connection.py

---
+++ client/connection.py
@@ -0,0 +1,16 @@
+import socket, pickle
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((input("IP Adress of server: "), 4000))
+
+def send(data):
+	data = pickle.dumps(data)
+	server.send(data)
+
+while True:
+  send(input())
+	data = server.recv(1024)
+	data = pickle.loads(data)
+	print(data)
+
+server.close()
