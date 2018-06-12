227b6f625a2b7bc656a9535bcdde4ac98b3d2250
Muffin1634 <muffin.1634@gmail.com>
2018-06-11 19:44:57 -0700

    Update connection.py

--- client/connection.py
+++ client/connection.py
@@ -1,7 +1,7 @@
 import socket, pickle
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((input("IP Adress of server: "), 4000))
+server.connect((input("IP Address of server: "), 4000))
 
 def send(data):
 	data = pickle.dumps(data)
@@ -9,8 +9,8 @@ def send(data):
 if __name__ == "__main__":
 	while True:
 		send(input())
-		#data = server.recv(1024)
-		#data = pickle.loads(data)
-		#print(data)
-  
+		# data = server.recv(1024)
+		# data = pickle.loads(data)
+		# print(data)
+
 server.close()
